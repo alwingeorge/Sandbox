35ff61c6bc2297a411ff1de3f3674b4188b8b782
alwingeorge <alwin.george@my.jcu.edu.au>
2018-08-16 13:42:23 +1000

    first version

--- password_entry.py
+++ password_entry.py
@@ -1,28 +1,8 @@
 # Alwin George
-out_file = open("name.txt", "w")
-name = input("What is your name? ")
-print(name, file=out_file)
-out_file.close()
+min_length = 4
 
+password = input(("Enter password of at least {} characters: ".format(min_length)))
+while len(password) < min_length:
+    password = input("Enter password of at least {} characters: ".format(min_length))
 
-in_file = open("name.txt", "r")
-name = in_file.read().strip()
-print("Your name is", name)
-in_file.close()
-
-
-in_file = open("numbers.txt", "r")
-number1 = int(in_file.readline())
-number2 = int(in_file.readline())
-print(number1 + number2)
-in_file.close()
-
-in_file = open("num.txt", "r")
-total = 0
-for line in in_file:
-    number = int(line)
-    total += number
-print(total)
-in_file.close()
-
-
+print('*' * len(password))
