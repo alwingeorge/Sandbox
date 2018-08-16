b47078cb8e33bae9f4fe11bf718ee8bfc75f525c
alwingeorge <alwin.george@my.jcu.edu.au>
2018-08-16 13:48:14 +1000

    Refactor password program to use functions

--- password_entry.py
+++ password_entry.py
@@ -2,7 +2,14 @@
 min_length = 4
 
 password = input(("Enter password of at least {} characters: ".format(min_length)))
-while len(password) < min_length:
-    password = input("Enter password of at least {} characters: ".format(min_length))
+
+
+def get_password():
+    global password
+    while len(password) < min_length:
+        password = input("Enter password of at least {} characters: ".format(min_length))
+
+
+get_password()
 
 print('*' * len(password))
