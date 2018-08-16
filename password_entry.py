cc13e5eb7f44ecfc131b48f7fdf75162c3922f54
alwingeorge <alwin.george@my.jcu.edu.au>
2018-08-16 13:25:58 +1000

    add code

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,28 @@
 # Alwin George
+out_file = open("name.txt", "w")
+name = input("What is your name? ")
+print(name, file=out_file)
+out_file.close()
+
+
+in_file = open("name.txt", "r")
+name = in_file.read().strip()
+print("Your name is", name)
+in_file.close()
+
+
+in_file = open("numbers.txt", "r")
+number1 = int(in_file.readline())
+number2 = int(in_file.readline())
+print(number1 + number2)
+in_file.close()
+
+in_file = open("num.txt", "r")
+total = 0
+for line in in_file:
+    number = int(line)
+    total += number
+print(total)
+in_file.close()
+
+
